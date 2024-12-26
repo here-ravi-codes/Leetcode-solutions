304c7e998338672d7a956164cf5ec1c63f227910
here-ravi-codes <289132849+here-ravi-codes@users.noreply.github.com>
2024-12-26 17:24:32 +0000

    solved leetcode (#125)

--- solutions/0125-valid-palindrome/README.md
+++ solutions/0125-valid-palindrome/README.md
@@ -1,6 +1,6 @@
 # Valid Palindrome
 
-- Submission ID: `1489059945`
-- Accepted: 2024-12-26T17:06:53Z
+- Submission ID: `1489075863`
+- Accepted: 2024-12-26T17:24:32Z
 - Language: java
 - Problem: https://leetcode.com/problems/valid-palindrome/
--- solutions/0125-valid-palindrome/solution.java
+++ solutions/0125-valid-palindrome/solution.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
-                 sb.append(Character.toLowerCase(ch));
-            }
-        }
         int i = 0;
-        int j = sb.length() - 1;
+        int j = s.length() - 1;
         while(i < j){
-            if(sb.charAt(i++) != sb.charAt(j--)){
-                return false;
+            while( i < j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
             }
+            while( i < j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))){
+                return false;
+            } 
         }
         return true;
     }

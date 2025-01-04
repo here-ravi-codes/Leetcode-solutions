5be8ff8594ff1c1a749ac4603e1532e6e78a4a16
LeetCode Sync <leetcode-sync@example.com>
2025-01-04 07:07:49 +0000

    Add Single Number (#136)

---
+++ solutions/single-number/136-single-number-1497045459.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor1 = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            xor1 = xor1 ^ nums[i];
+        }
+        return xor1;
+    }
+}

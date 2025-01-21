b73a69df8de4ab0c443f0fe0cd155271e8e00f95
here-ravi-codes <whereicode@gmail.com>
2025-01-21 18:27:51 +0000

    Add Isomorphic Strings (#205)

---
+++ solutions/isomorphic-strings/205-isomorphic-strings-1516077325.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> mapS = new HashMap<>();
+        HashMap<Character, Character> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check forward mapping: s -> t
+            if (mapS.containsKey(charS)) {
+                if (mapS.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                mapS.put(charS, charT);
+            }
+
+            // Check reverse mapping: t -> s
+            if (mapT.containsKey(charT)) {
+                if (mapT.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                mapT.put(charT, charS);
+            }
+        }
+
+        return true;
+    }
+}

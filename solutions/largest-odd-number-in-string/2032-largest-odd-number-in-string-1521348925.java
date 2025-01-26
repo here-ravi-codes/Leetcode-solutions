bd4d1ab138490a753cc369e9cf41557ac9f15a75
LeetCode Sync <leetcode-sync@example.com>
2025-01-26 18:02:48 +0000

    Add Largest Odd Number In String (#2032)

---
+++ solutions/largest-odd-number-in-string/2032-largest-odd-number-in-string-1521348925.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int i;
+        for(i = num.length()-1; i >= 0; i--){
+            if((num.charAt(i) - '0') % 2 != 0){
+                break;
+            }
+        }
+        if(i < 0){
+            return "";
+        }
+        return num.substring(0,i+1);
+    }
+}

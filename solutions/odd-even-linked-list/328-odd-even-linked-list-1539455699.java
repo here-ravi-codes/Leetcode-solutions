7439973efebd18a4d820440c5511f6c6a5957944
here-ravi-codes <whereicode@gmail.com>
2025-02-11 16:02:53 +0000

    Add Odd Even Linked List (#328)

---
+++ solutions/odd-even-linked-list/328-odd-even-linked-list-1539455699.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        // if(head == null || head.next == null) return head;
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null && temp.next != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null){
+        //     arr.add(temp.val);
+        // }
+        // temp = head.next;
+        // while(temp != null && temp.next != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null){
+        //     arr.add(temp.val);
+        // }
+        // temp = head;
+        // int i = 0;
+        // while(temp != null){
+        //     temp.val = arr.get(i);
+        //     i++;
+        //     temp = temp.next;
+        // }
+        // return head;
+
+
+        if(head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            even = even.next;
+            odd = odd.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

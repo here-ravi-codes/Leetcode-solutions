55ec6866d0a0b52dcdc937a6dc7817a8218f1319
LeetCode Sync <leetcode-sync@example.com>
2025-03-16 17:54:48 +0000

    Add Longest Palindromic Subsequence (#516)

---
+++ solutions/longest-palindromic-subsequence/516-longest-palindromic-subsequence-1575934627.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String s1 = new StringBuilder(s).reverse().toString();
+        int m = s.length();
+        int n = s1.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 0; i < m + 1; i++){
+            for(int j = 0; j < n + 1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(s.charAt(i - 1) == s1.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

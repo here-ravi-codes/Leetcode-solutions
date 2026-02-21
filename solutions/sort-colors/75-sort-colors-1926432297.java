a3e0030748e127ff151def335d763ad20036014d
LeetCode Sync <leetcode-sync@example.com>
2026-02-21 14:59:17 +0000

    Add Sort Colors (#75)

---
+++ solutions/sort-colors/75-sort-colors-1926432297.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int m = 0;
+        int h = n - 1;
+        while(m <= h){
+            if(nums[m] == 0){
+                swap(nums, l, m);
+                l++;
+                m++;
+            }
+            else if(nums[m] == 1) m++;
+            else{
+                swap(nums, m, h);
+                h--;
+            }
+        }
+    }
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}

2b16e7a44792acbe58e2530ac8525ff95ba02686
here-ravi-codes <whereicode@gmail.com>
2026-06-17 05:20:03 +0000

    Add Hand Of Straights (#876)

---
+++ solutions/hand-of-straights/876-hand-of-straights-2035901298.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if(n % groupSize != 0) return false;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int i = 0; i < n; i++){
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
+        }
+        while(!map.isEmpty()){
+            int start = map.firstKey();
+            for(int i = start; i < start + groupSize; i++){
+                if(!map.containsKey(i)) return false;
+                map.put(i, map.get(i) - 1);
+                if(map.get(i) == 0) map.remove(i);
+            }
+        }
+        return true;
+    }
+}

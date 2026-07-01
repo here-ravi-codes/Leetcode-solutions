791b6112b67bbaaae423526148b634cd9bb6e1b5
LeetCode Sync <leetcode-sync@example.com>
2026-07-01 05:53:47 +0000

    Add Best Time To Buy And Sell Stock (#121)

---
+++ solutions/best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock-2051939360.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++){
+            int profit = prices[i] - buy;
+            maxProfit = Math.max(maxProfit, profit);
+            buy = Math.min(buy, prices[i]);
+        }
+        return maxProfit;
+    }
+}

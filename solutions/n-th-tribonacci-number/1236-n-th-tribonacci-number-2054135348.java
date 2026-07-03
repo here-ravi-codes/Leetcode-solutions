489329f372e628bd9a27c38a36e2573c1b0347fa
LeetCode Sync <leetcode-sync@example.com>
2026-07-03 03:44:13 +0000

    Add N Th Tribonacci Number (#1236)

---
+++ solutions/n-th-tribonacci-number/1236-n-th-tribonacci-number-2054135348.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return n;
+        int a = 0; 
+        int b = 1;
+        int c = 1;
+        for(int i = 3; i <= n; i++){
+            int temp = a + b + c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
+    }
+}
